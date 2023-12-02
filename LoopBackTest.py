fa64c5e15d86a39f4832eba081a075503cae6e61
ColinPollard <colin.pollard1466@gmail.com>
2023-12-01 18:52:20 -0700

    Fix 1

--- LoopBackTest.py
+++ LoopBackTest.py
@@ -6,7 +6,7 @@ ser.baudrate = 115200
 
 while True:
     print('Sending: Test')
-    ser.write("Test\n")
+    ser.write(b'Test\n')
     received = ser.readline()
     print('Received: ', received)
     time.sleep(1)
